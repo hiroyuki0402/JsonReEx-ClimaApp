d6234359d4a60c740fe0301cd4a51eeaf3f73f82
hiroyuki0402 <88645049+hiroyuki0402@users.noreply.github.com>
2022-05-20 23:52:10 +0900

    [clean] 整理

--- ClimaApp-JsonReEx/Model/WeatherManager.swift
+++ ClimaApp-JsonReEx/Model/WeatherManager.swift
@@ -12,10 +12,27 @@ import UserNotifications
 enum URLMode {
     case ctyName(String)
     case location(lat: CLLocationDegrees, lon: CLLocationDegrees)
+
+    func getUrl() -> URL? {
+        switch self {
+        case .ctyName(let string):
+            let encodedString = String().encoderString(word: string) ?? ""
+            return URL(string:  WeatherManager.Url.url + "&q=\(encodedString)")
+        case .location(let lat, let lon):
+            return URL(string: WeatherManager.Url.url + "&lat=\(lat)&lon=\(lon)")
+        }
+    }
 }
 
 enum ErrorType: Error {
     case testErr
+}
+
+private extension String {
+    func encoderString(word: String) -> String? {
+        let encodeString = word.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        return encodeString
+    }
 }
 
 struct WeatherManager {
@@ -25,34 +42,16 @@ struct WeatherManager {
         static let openweatherString = "api.openweathermap.org/data/2.5/weather?units=metric&"
         static let url = internetProtocol + openweatherString + api
     }
-    var weatherUrl = Url.url
-    /// URLのモードの判定
-    /// - Parameter mode: URLMode
-    /// - Returns: modeのURLの文字列
-    func fetchWeather(mode: URLMode) -> String {
-        var url: String
-        // モードの判定
-        switch mode {
-            // 地域名
-        case .ctyName(let string):
-            url = "\(weatherUrl)&q=\(string)"
-            // 現在地
-        case .location(let lat, let lon):
-            url = "\(weatherUrl)&lat=\(lat)&lon=\(lon)"
-        }
-        return url
-    }
-
     /// 天気取得
     /// - Parameter url: TextFieldで入力した地域,国
     /// - Returns: AcquisitionTargetAtItemの配列(Codble)
-    func performReqest(url: String) async throws -> [AcquisitionTargetAtItem] {
-        guard let url = URL(string: url) else {
+    func performReqest(url: URL?) async throws -> [AcquisitionTargetAtItem] {
+        guard let url = url else {
             throw URLError(.badURL)
         }
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let item = try JSONDecoder().decode(AcquisitionTargetAtItem.self, from: data)
-            return [item]
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let item = try JSONDecoder().decode(AcquisitionTargetAtItem.self, from: data)
+        return [item]
     }
 }
 
